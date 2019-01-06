b8b449cf6f7d4070a660852d567f1db95b7811f3
daslu <daniel.slutsky@gmail.com>
2019-01-06 13:19:50 +0200

    clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [alembic "0.3.2"]
                  [org.clojars.benfb/gorilla-repl "0.5.3"]
                  [gorilla-plot "0.1.4"]]
